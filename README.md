1f7d8957c480bb90d1fcb21b11ac52a75a266ebf
yanzhidong <yanzhidong@sm.vvip-u.com>
2020-07-29 17:49:27 +0800

    排序

--- README.md
+++ README.md
@@ -33,3 +33,36 @@
  * [棒球比赛(682)](/src/main/java/com/yzd/algorithm/stack/BaseballGame.java) 
 
  * [下一个更大元素I(496)](/src/main/java/com/yzd/algorithm/stack/NextGreaterElementI.java) 
+
+[排序](/src/main/java/com/yzd/algorithm/sort)
+
+ * [冒泡排序](/src/main/java/com/yzd/algorithm/sort/BubbleSort.java)
+
+ * [插入排序](/src/main/java/com/yzd/algorithm/sort/InsertionSort.java)
+   - 冒泡排序和插入排序的时间复杂度都是O(n2)，都是原地排序算法，都是稳定的排序算法。
+   - 冒泡排序插和入排序不管怎么优化，元素交换的次数是一个固定值，是原始数据的逆序度。
+   - 为什么插入排序要比冒泡排序更受欢迎呢？
+     - 代码实现上来看，冒泡排序的数据交换要比插入排序的数据移动要复杂，冒泡排序需要 3 个赋值操作，而插入排序只需要 1 个
+   ```java
+        //冒泡排序中数据的交换操作：
+        if (a[j] > a[j+1]) { // 交换
+           int tmp = a[j];
+           a[j] = a[j+1];
+           a[j+1] = tmp;
+           flag = true;
+        }   
+        //插入排序中数据的移动操作：
+        if (a[j] > value) {
+          a[j+1] = a[j];  // 数据移动
+        } else {
+          break;
+        }
+   ```
+ 
+ * [归并排序](/src/main/java/com/yzd/algorithm/sort/MergeSort.java)
+   - 并归排序是稳定的排序算法不是原地排序，时间复杂度是O(nlogn) 空间复杂度是 O(n)
+
+ * [快速排序](/src/main/java/com/yzd/algorithm/sort/QuickSort.java)
+   - 快排是一种原地、不稳定的排序算法，间复杂度也是 O(nlogn)
+
+
---
+++ src/main/java/com/yzd/algorithm/sort/MergeSort.java
@@ -0,0 +1,70 @@
+package com.yzd.algorithm.sort;
+
+import java.util.Arrays;
+
+/***
+ *
+ * 归并排序算法步骤
+ *  1. 申请空间，使其大小为两个已经排序序列之和，该空间用来存放合并后的序列；
+ *  2. 设定两个指针，最初位置分别为两个已经排序序列的起始位置；
+ *  3. 比较两个指针所指向的元素，选择相对小的元素放入到合并空间，并移动指针到下一位置；
+ *  4. 重复步骤 3 直到某一指针达到序列尾；
+ *  5. 将另一序列剩下的所有元素直接复制到合并序列尾。
+ *
+ *
+ * @author : yanzhidong
+ * @date : 2020/4/25 
+ * @version : V1.0
+ *
+ */
+public class MergeSort {
+
+
+    public static void main(String[] args) {
+        int[] arr = {38, 46, 15, 6, 90, 99, 11, 5, 3, 100};
+        //新建一个临时数组存放
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, 0, arr.length - 1, tmp);
+        Arrays.stream(arr).forEach(System.out::println);
+
+    }
+
+    public static void mergeSort(int[] arr, int low, int high, int[] tmp) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            //对左边序列进行归并排序
+            mergeSort(arr, low, mid, tmp);
+            //对右边序列进行归并排序
+            mergeSort(arr, mid + 1, high, tmp);
+            //合并两个有序序列
+            merge(arr, low, mid, high, tmp);
+        }
+    }
+
+    public static void merge(int[] arr, int low, int mid, int high, int[] tmp) {
+        int i = 0;
+        //左边序列和右边序列起始索引
+        int j = low, k = mid + 1;
+        while (j <= mid && k <= high) {
+            if (arr[j] < arr[k]) {
+                tmp[i++] = arr[j++];
+            } else {
+                tmp[i++] = arr[k++];
+            }
+        }
+        //若左边序列还有剩余，则将其全部拷贝进tmp[]中
+        while (j <= mid) {
+            tmp[i++] = arr[j++];
+        }
+
+        while (k <= high) {
+            tmp[i++] = arr[k++];
+        }
+
+        for (int t = 0; t < i; t++) {
+            arr[low + t] = tmp[t];
+        }
+    }
+
+
+}
